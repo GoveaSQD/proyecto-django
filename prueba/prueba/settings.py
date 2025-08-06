c3ba4076a084aaf79a1753c091c8502fa93a06e3
Govea <ligthgovea@gmail.com>
2025-08-05 21:12:37 -0600

    Archivo requirements

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'inicio',  # aplicación personalizada
     'registros.apps.RegistrosConfig',  # aplicación personalizada
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
