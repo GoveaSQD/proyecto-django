24b2aadf11e48c854f01413bfeb17adc73826f19
GoveaSQD <120681867+GoveaSQD@users.noreply.github.com>
2025-08-06 10:50:22 -0600

    Master (#3)
    * Archivo requirements
    
    * Actualizacion completa

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
-from .models import Alumnos
-from .models import Comentario
-class AdministrarModelo(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
-    list_display=('matricula','nombre','carrera','turno')
-    search_fields = ('matricula', 'nombre', 'carrera', 'turno')
 
-admin.site.register(Alumnos, AdministrarModelo)
-class AlumnosAdmin(admin.ModelAdmin):
-    list_display = ('matricula', 'nombre', 'carrera', 'turno', 'created', 'updated')
-    search_fields = ('matricula', 'nombre', 'carrera', 'turno')
-    list_filter = ('turno',)
-    ordering = ('-created',)
-# Register your models here.
+from .models import ComentarioContacto
 
-class AdministrarComentarios(admin.ModelAdmin):
-    list_display = ('id', 'coment')
-    search_fields = ('id', 'created')
+class AdministrarComentariosContacto(admin.ModelAdmin):
+    list_display = ('id', 'mensaje')
+    search_fields = ('id','created')
     date_hierarchy = 'created'
     readonly_fields = ('created', 'id')
+    
 
-admin.site.register(Comentario, AdministrarComentarios)
+admin.site.register(ComentarioContacto, AdministrarComentariosContacto)
